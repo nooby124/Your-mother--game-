a562a22e3b59557c6e9ca97262528227fb7feb63
nooby124 <139121928+nooby124@users.noreply.github.com>
2023-07-10 19:58:14 +0300

    Create your-mother.py

---
+++ your-mother.py
@@ -0,0 +1,11 @@
+import time
+sussy_baka = input('great argument however... ')
+if sussy_baka == 'upupdowndownleftritleafttitsdhfjsdfhkjfdshgfdugyhufdygrhjksouseeigottomyminecrap':
+    print('how the fuck')
+    time.sleep(5)
+if sussy_baka == 'never gonna give you up nevr gonna let yu down':
+    print('bro why')
+    time.sleep(5)
+else:
+    while True:
+        print('your mother ')
