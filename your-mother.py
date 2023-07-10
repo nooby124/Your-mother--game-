a828dd11e9ce19d932783087dc7a43253b34d8d6
nooby124 <139121928+nooby124@users.noreply.github.com>
2023-07-10 21:10:18 +0300

    added some more easter eggs

--- your-mother.py
+++ your-mother.py
@@ -3,11 +3,16 @@ sussy_baka = input('great argument however... ')
 if sussy_baka == 'your mother':
     while True:
         print('no you')
-if sussy_baka
+        time.sleep(0.5)
+if sussy_baka == 'revewoh tnemugra taerg':
+    print('?felsruoy ot siht od uoy did yhw'
+if sussy_baka == 'i am better than u at fortnite' or 'i am better than you at fortnite':
+    print('i dont even play fortnite')
+    time.sleep(0.5)
 if sussy_baka == 'upupdowndownleftritleafttitsdhfjsdfhkjfdshgfdugyhufdygrhjksouseeigottomyminecrap':
     print('why are u gae')
     time.sleep(5)
-if sussy_baka == 'never gonna give you up nevr gonna let yu down':
+if sussy_baka == 'never gonna give you up never gonna let you down':
     print('bro why')
     time.sleep(5)
 else:
